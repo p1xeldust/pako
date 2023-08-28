fe41047a221062abe5bd899b73c1b15593d74c75
Paul Goldstein <81930845+toastmakingmachine@users.noreply.github.com>
2023-08-29 02:12:50 +0300

    Add files via upload
    deps detection fix

--- src/remove.cpp
+++ src/remove.cpp
@@ -8,34 +8,27 @@ namespace fs = std::filesystem;
 
 bool solveDeps(string packageName) {
     vector<string> dependencies;
-    #ifdef DEBUG
-        cout << "Solver active" << endl;
-    #endif
-    
-    for(const auto entry : fs::directory_iterator((string) VAR_PATH + "/packages")) {   
+    for(const auto entry : fs::directory_iterator((string)VAR_PATH + "/packages")) {
+        if(entry.path().string() == (string)VAR_PATH + "/packages/" + packageName)
+            continue;
+        cout << entry.path().string() << endl;
         ifstream dataFile(entry.path().string() + "/info", ios::in);
+    
+        for(string line; getline(dataFile, line); dataFile.good()) {
 
-	    for(string line; getline(dataFile, line); dataFile.good()) {
-
-    	    if(line.find(':') != std::string::npos) {
-	    		string type = line.substr(0, line.find(':'));
-    			string value = line.substr(line.find(':') + 1);
-
-			    type.erase(type.find_last_not_of(" \t") + 1);
-		     	value.erase(0, value.find_first_not_of(" \t"));
-
-	    		if(type == "deps" && value == packageName) {
-       			    #ifdef DEBUG
-                        cout << "[Debug:checkDeps] Dependency found: '" << entry.path().string().erase(0, entry.path().string().find_last_of("/")+1) << "'" << endl;
-               	    #endif
-				    dependencies.push_back(std::move(entry.path().string().erase(0, entry.path().string().find_last_of("/")+1)));
-                    break;
-    		    } else
-			    	continue;
+	    	if(line.find("deps:") != std::string::npos) {
+		    	istringstream dependenciesList(line.substr(line.find(':') + 1).erase(0, line.find_first_not_of(" \t")));
+			    string dependency;
+		    	for(string dependency; dependenciesList >> dependency;)
+                    if(dependency == packageName)
+					    dependencies.push_back(std::move(entry.path().string().substr(entry.path().string().find_last_of("/") +1)));
 		    }
 	    }
+        
     }
-	if(dependencies.size() <= 0) {
+    
+
+    if(dependencies.size() <= 0) {
         #ifdef DEBUG
             cout << "[Debug.SolveDeps] Solved dependencies!" << endl;
         #endif
@@ -44,8 +37,8 @@ bool solveDeps(string packageName) {
         cout << "[i] Unable to solve dependencies. Packages";
 
         for(size_t i=0; i < dependencies.size(); i++)
-            cout << " " << dependencies[i];
-        cout << " require " << packageName << endl;
+            cout << " '" << dependencies[i] << "'";
+        cout << " needs " << packageName << endl;
         cout << "Use --force-remove to avoid dependencies check." << endl;
         return false;
     }
@@ -54,7 +47,7 @@ void removePackageSource(string listFilePath) {
     vector<string> files;
     ifstream listFile(listFilePath);
     for(string line; getline(listFile, line); listFile.good())
-        files.push_back(std::move((string) PREFIX + "/" + line));
+        files.push_back(std::move((string)PREFIX + "/" + line));
     for(size_t i; i<files.size(); i++)
         if(fs::exists(files[i]) && fs::is_regular_file(files[i]))
             fs::remove(files[i]);
@@ -95,7 +88,6 @@ uint8_t Remove(int argc, char* argv[]) {
     cout << "[i] Preparing " << argv[i] << endl;
     if(!flag_force_remove)
         if(!solveDeps(packageInfo[0])) {
-            cout << "Package is not installed"  << endl;
             return 2;
         }
     
